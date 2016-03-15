beebcadcc8c4c0b001870364c0205f1652582c34
Xellath <alexanderjoh94@gmail.com>
2016-03-15 12:03:44 +0100

    Heh

--- app/src/main/java/com/grupp32/freeelo/MainActivity.java
+++ app/src/main/java/com/grupp32/freeelo/MainActivity.java
@@ -2,6 +2,9 @@ package com.grupp32.freeelo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn = (Button) findViewById(R.id.okButton);
+        final TextView txt = (TextView) findViewById(R.id.summonerName);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
