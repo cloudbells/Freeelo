461bfdc4efb773dadd4dfac59d417626d442818b
Xellath <alexanderjoh94@gmail.com>
2016-03-16 15:03:09 +0100

    LoginUI

--- app/src/main/java/com/grupp32/freeelo/LoginActivity.java
+++ app/src/main/java/com/grupp32/freeelo/LoginActivity.java
@@ -1,7 +1,15 @@
 package com.grupp32.freeelo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +17,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+        Button btnAlpha = (Button) findViewById(R.id.okButton);
+
+        assert btnAlpha != null;
+        btnAlpha.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        View v = getCurrentFocus();
+        boolean ret = super.dispatchTouchEvent(event);
+
+        if (v instanceof EditText) {
+            View w = getCurrentFocus();
+            int scrCoords[] = new int[2];
+            w.getLocationOnScreen(scrCoords);
+            float x = event.getRawX() + w.getLeft() - scrCoords[0];
+            float y = event.getRawY() + w.getTop() - scrCoords[1];
+
+            if(event.getAction() == MotionEvent.ACTION_UP && (x < w.getLeft() || x >= w.getRight() || y < w.getTop() || y > w.getBottom())) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getWindow().getCurrentFocus().getWindowToken(), 0);
+            }
+        }
+
+        return ret;
     }
 }
