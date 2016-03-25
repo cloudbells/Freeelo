2c30cd90b1156ca0b8dc8de87373b74156e4a720
Xellath <alexanderjoh94@gmail.com>
2016-03-25 12:28:02 +0100

    Start på MainActivity

--- app/src/main/java/com/grupp32/freeelo/MainActivity.java
+++ app/src/main/java/com/grupp32/freeelo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.grupp32.freeelo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Top").setIcon(R.drawable.ic_top));
+        tabLayout.addTab(tabLayout.newTab().setText("Jungle").setIcon(R.drawable.ic_jgl));
+        tabLayout.addTab(tabLayout.newTab().setText("Mid").setIcon(R.drawable.ic_mid));
+        tabLayout.addTab(tabLayout.newTab().setText("Bot").setIcon(R.drawable.ic_bot));
+        tabLayout.addTab(tabLayout.newTab().setText("Supp").setIcon(R.drawable.ic_sup));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+    }
+
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
+
+        public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+            super(fm);
+            this.mNumOfTabs = NumOfTabs;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    TabFragment tab1 = new TabFragment();
+                    return tab1;
+                case 1:
+                    TabFragment tab2 = new TabFragment();
+                    return tab2;
+                case 2:
+                    TabFragment tab3 = new TabFragment();
+                    return tab3;
+                case 3:
+                    TabFragment tab4 = new TabFragment();
+                    return tab4;
+                case 4:
+                    TabFragment tab5 = new TabFragment();
+                    return tab5;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mNumOfTabs;
+        }
     }
 }
---
+++ app/src/main/java/com/grupp32/freeelo/TabFragment.java
@@ -0,0 +1,53 @@
+package com.grupp32.freeelo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TabFragment extends Fragment {
+    private ImageView background;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab, container, false);
+        background = (ImageView) view.findViewById(R.id.background);
+
+        try {
+            new BackgroundSwitcher().execute(new URL("http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+    private class BackgroundSwitcher extends AsyncTask<URL, Bitmap, Bitmap> {
+        protected Bitmap doInBackground(URL... urls) {
+            Bitmap bitmap = null;
+            URL url = urls[0];
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream) url.getContent());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            background.setImageBitmap(Bitmap.createScaledBitmap(bitmap, background.getWidth(), background.getHeight(), false));
+        }
+    }
+}
