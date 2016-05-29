2d1105f65625227f405696eba954ca18c4e2eda2
Xellath <alexanderjoh94@gmail.com>
2016-05-29 16:51:33 +0200

    Javadoc + reformat

--- app/src/main/java/version/ResourceUtil.java
+++ app/src/main/java/version/ResourceUtil.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Created by Christoffer Nilsson on 2016-05-12.
+ * ResourceUtil handles writing to internal storage.
+ *
+ * @author Christoffer Nilsson
  */
 public class ResourceUtil {
 
@@ -20,32 +22,67 @@ public class ResourceUtil {
     private JSONObject runes;
     private JSONObject spells;
 
+    /**
+     * Constructor for ResourceUtil. Parameters accept application context.
+     *
+     * @param context application context
+     */
     public ResourceUtil(Context context) {
         this.context = context;
     }
 
+    /**
+     * Writes to a specified resource file in internal storage.
+     *
+     * @param resourceName resource file
+     * @param content      content to write
+     * @throws IOException
+     */
     public void writeResource(String resourceName, String content) throws IOException {
-        FileOutputStream outputStream = context.openFileOutput(resourceName, Context.MODE_PRIVATE);
-        outputStream.write(content.getBytes());
+        FileOutputStream outputStream = context.openFileOutput(resourceName, Context.MODE_PRIVATE); // Open stream
+        outputStream.write(content.getBytes()); // Write content
         outputStream.close();
     }
 
+    /**
+     * Returns a JSONObject from built resource originated from champions.json.
+     *
+     * @return <code>JSONObject</code> - JSONObject from champions.json
+     */
     public JSONObject getChampions() {
         return champions;
     }
 
+    /**
+     * Returns a JSONObject from built resource originated from masteries.json.
+     *
+     * @return <code>JSONObject</code> - JSONObject from masteries.json
+     */
     public JSONObject getMasteries() {
         return masteries;
     }
 
+    /**
+     * Returns a JSONObject from built resource originated from runes.json.
+     *
+     * @return <code>JSONObject</code> - JSONObject from runes.json
+     */
     public JSONObject getRunes() {
         return runes;
     }
 
+    /**
+     * Returns a JSONObject from built resource originated from spells.json.
+     *
+     * @return <code>JSONObject</code> - JSONObject from spells.json
+     */
     public JSONObject getSpells() {
         return spells;
     }
 
+    /**
+     * Builds and stores JSONObjects from resources in internal storage.
+     */
     public void buildResources() {
         try {
             champions = buildResource("champions.json");
@@ -57,6 +94,14 @@ public class ResourceUtil {
         }
     }
 
+    /**
+     * Builds resources from internal storage.
+     *
+     * @param resource resource to build
+     * @return <code>JSONObject</code> - JSONObject from resource
+     * @throws IOException   if the URL is incorrect for the API
+     * @throws JSONException if returned data is corrupt or parsed wrong
+     */
     private JSONObject buildResource(String resource) throws IOException, JSONException {
         Scanner scanner = new Scanner(context.openFileInput(resource));
         String str = "";
--- app/src/main/java/version/VersionUtil.java
+++ app/src/main/java/version/VersionUtil.java
@@ -1,21 +1,20 @@
 package version;
 
 import android.content.Context;
-import android.util.Log;
-import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import json.JSONParser;
 import json.JSONRequester;
 
 /**
- * Created by Christoffer Nilsson on 2016-05-12.
+ * VersionUtil checks, parses and saves static patch data using several other classes.
+ *
+ * @author Christoffer Nilsson
  */
 public class VersionUtil {
 
@@ -30,6 +29,15 @@ public class VersionUtil {
     private JSONRequester requester;
     private ResourceUtil resourceUtil;
 
+    /**
+     * Constructor for VersionUtil, accepts context and certain needed objects for it to work.
+     *
+     * @param context      application context
+     * @param parser       JSONParser-object
+     * @param requester    JSONRequester-object
+     * @param resourceUtil ResourceUtil-object
+     * @param region       region to check against
+     */
     public VersionUtil(Context context, JSONParser parser, JSONRequester requester, ResourceUtil resourceUtil, String region) {
         this.context = context;
         this.parser = parser;
@@ -37,35 +45,46 @@ public class VersionUtil {
         this.resourceUtil = resourceUtil;
         this.region = region.toLowerCase();
         try {
+            // If not first time
             if (!isFirstTime()) {
+                // Build resources from internal storage
                 resourceUtil.buildResources();
+                // Get static data versions
                 champVersion = resourceUtil.getChampions().getString("version");
                 masteryVersion = resourceUtil.getMasteries().getString("version");
                 runeVersion = resourceUtil.getRunes().getString("version");
                 spellVersion = resourceUtil.getSpells().getString("version");
             }
 
+            // Set latest patch
             latestPatch = requester.requestPatchData(this.region).getString(0);
         } catch (JSONException | IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Updates current static data version.
+     */
     public void updateVersion() {
         try {
+            // Request all static data one-by-one
             JSONObject champions = requester.requestStaticChampionData(region);
             JSONObject masteries = requester.requestStaticMasteryData(region);
             JSONObject runes = requester.requestStaticRuneData(region);
             JSONObject spells = requester.requestStaticSpellData(region);
+            // Get versions
             champVersion = champions.getString("version");
             masteryVersion = masteries.getString("version");
             runeVersion = runes.getString("version");
             spellVersion = spells.getString("version");
+            // Write as a resource to internal storage
             resourceUtil.writeResource("champions.json", champions.toString());
             resourceUtil.writeResource("masteries.json", masteries.toString());
             resourceUtil.writeResource("runes.json", runes.toString());
             resourceUtil.writeResource("spells.json", spells.toString());
 
+            // Build and update
             resourceUtil.buildResources();
             parser.updateResources();
         } catch (JSONException | IOException e) {
@@ -73,6 +92,11 @@ public class VersionUtil {
         }
     }
 
+    /**
+     * Method checks if internal storage contains static data, if not, returns a true boolean.
+     *
+     * @return <code>boolean</code> - true if first time (has no static data), false if static data exists
+     */
     public boolean isFirstTime() {
         File champions = context.getFileStreamPath("champions.json");
         File masteries = context.getFileStreamPath("masteries.json");
@@ -84,7 +108,12 @@ public class VersionUtil {
         return false;
     }
 
-    public boolean isLatestVersion()  {
+    /**
+     * Method checks if current static data is up-to-date (latest version).
+     *
+     * @return <code>boolean</code> - true if latest patch, false if not
+     */
+    public boolean isLatestVersion() {
         if (champVersion.equals(latestPatch) && masteryVersion.equals(latestPatch) &&
                 runeVersion.equals(latestPatch) && spellVersion.equals(latestPatch)) {
             return true;
@@ -92,6 +121,11 @@ public class VersionUtil {
         return false;
     }
 
+    /**
+     * Returns current patch version.
+     *
+     * @return <code>String</code> - current patch version, ie. "6.10"
+     */
     public String getVersion() {
         return latestPatch;
     }
