2d7b915d68067740e059d8a2871a716f926eb87a
Xellath <alexanderjoh94@gmail.com>
2016-05-29 14:34:51 +0200

    ghjg

--- app/src/main/java/collection/Ultimate.java
+++ app/src/main/java/collection/Ultimate.java
@@ -9,47 +9,6 @@ import java.io.Serializable;
  */
 public class Ultimate implements Serializable {
 
-	private int maxRank;
-	private double[] cooldowns;
-	private String name;
-	private String imageName;
-
-	public Ultimate setMaxRank(int maxRank) {
-		this.maxRank = maxRank;
-		return this;
-	}
-
-	public Ultimate setCooldowns(double[] cooldowns) {
-		this.cooldowns = cooldowns;
-		return this;
-	}
-
-	public Ultimate setName(String name) {
-		this.name = name;
-		return this;
-	}
-
-	public Ultimate setImageName(String imageName) {
-		this.imageName = imageName;
-		return this;
-	}
-
-	public int getMaxRank() {
-		return maxRank;
-	}
-
-	public double[] getCooldowns() {
-		return cooldowns;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getImageName() {
-		return imageName;
-	}
-
     private int maxRank;
     private double[] cooldowns;
     private String name;
