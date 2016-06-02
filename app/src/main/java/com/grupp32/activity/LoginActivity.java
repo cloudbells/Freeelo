19ed862e2c657924710f3b12d6e6fdc5ad50f48f
Xellath <alexanderjoh94@gmail.com>
2016-06-02 22:22:46 +0200

    Whoops?

--- app/src/main/java/com/grupp32/activity/LoginActivity.java
+++ app/src/main/java/com/grupp32/activity/LoginActivity.java
@@ -34,102 +34,128 @@ import version.ResourceUtil;
 import version.VersionUtil;
 
 /**
+ * LoginActivity represents the first landing page of Freeelo.
+ * The UI contains a spinner, a text field and a button that can be interacted with.
+ *
  * @author Alexander Johansson, Sigvard Nilsson
  */
 public class LoginActivity extends AppCompatActivity {
-	private AutoCompleteTextView twSummonerName;
-	private Spinner spRegion;
-	private CircularProgressButton btnSearch;
+
+	private AutoCompleteTextView summonerName;
+	private Spinner region;
+	private CircularProgressButton search;
 
 	private boolean searchedPressed = false;
 
-    private String[] autoCompleteArray;
-    private HashSet autoCompleteSet;
-    private SharedPreferences prefs;
+	private String[] autoCompleteArray;
+	private HashSet autoCompleteSet;
+	private SharedPreferences prefs;
 
 	private Summoner[] previouslySearchedSummonerArr;
 	private String previouslySearchedPatchVersion;
 	private String previouslySearchedSummoner;
-    private String previouslySearchedRegion;
+	private String previouslySearchedRegion;
 
 	private static final String ARG_SUMMONERS = "ARG_SUMMONERS";
 	private static final String ARG_SEARCHED_SUMMONER = "ARG_SEARCHED_SUMMONERS";
 	private static final String ARG_VERSION = "ARG_VERSION";
-    private static final String ARG_REGION = "ARG_REGION";
+	private static final String ARG_REGION = "ARG_REGION";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Set root view
 		setContentView(R.layout.activity_login);
 
-		btnSearch = (CircularProgressButton) findViewById(R.id.search_button);
-		btnSearch.setIndeterminateProgressMode(true);
-		btnSearch.setOnClickListener(new View.OnClickListener() {
+		// Find the button view, and assign it a listener
+		search = (CircularProgressButton) findViewById(R.id.search_button);
+		search.setIndeterminateProgressMode(true); // Sets the CircularProgressButton to be indeterminate (no 0-100 progress bar, always spinning)
+		search.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if(!searchedPressed && !checkPreviouslySearched()) {
+				if (!searchedPressed && !checkPreviouslySearched()) { // Check if previously searched, searchPressed check disables button spamming
 					searchedPressed = true;
-					btnSearch.setProgress(1);
+					search.setProgress(1);
 					checkCurrentGame();
 				}
 			}
 		});
 
-        setupSharedPrefs();
-        setupSearchBar();
+		setupSharedPrefs();
+		setupSearchBar();
 
-		spRegion = (Spinner) findViewById(R.id.region);
+		// Find spinner view and set its adapter from layout, also its content from strings (regions)
+		region = (Spinner) findViewById(R.id.region);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.regions, R.layout.textview_spinner);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spRegion.setAdapter(adapter);
+		region.setAdapter(adapter);
 
+		// Get intent (if forwarded from any previous page, in case of back-pressing from MainActivity)
 		Intent intent = this.getIntent();
 		Bundle bundle = intent.getExtras();
-		if(bundle != null) {
+		if (bundle != null) {
+			// Set data to match previous search from MainActivity from bundle
 			previouslySearchedSummonerArr = (Summoner[]) bundle.getSerializable(ARG_SUMMONERS);
 			previouslySearchedPatchVersion = bundle.getString(ARG_VERSION);
 			previouslySearchedSummoner = bundle.getString(ARG_SEARCHED_SUMMONER);
-            previouslySearchedRegion = bundle.getString(ARG_REGION);
-			twSummonerName.setText(previouslySearchedSummoner);
-            spRegion.setSelection(((ArrayAdapter)spRegion.getAdapter()).getPosition(previouslySearchedRegion));
+			previouslySearchedRegion = bundle.getString(ARG_REGION);
+			summonerName.setText(previouslySearchedSummoner);
+			region.setSelection(((ArrayAdapter) region.getAdapter()).getPosition(previouslySearchedRegion));
 		}
 	}
 
-    private void setupSharedPrefs() {
-        prefs = getSharedPreferences("autocomplete", MODE_PRIVATE);
-        HashSet hashSet = (HashSet) prefs.getStringSet("searchedNames", new HashSet<String>());
-        autoCompleteSet = (HashSet) hashSet.clone();
-        autoCompleteArray = new String[hashSet.size()];
-        Iterator it = hashSet.iterator();
-        int index = 0;
-        while (it.hasNext()) {
-            autoCompleteArray[index++] = (String) it.next();
-        }
-    }
+	/**
+	 * Setups SharedPreferences for Freeelo app, allows us to store summoner names that have been entered
+	 */
+	private void setupSharedPrefs() {
+		// Get any names previously entered
+		prefs = getSharedPreferences("autocomplete", MODE_PRIVATE);
+		HashSet hashSet = (HashSet) prefs.getStringSet("searchedNames", new HashSet<String>());
+		// Clone
+		autoCompleteSet = (HashSet) hashSet.clone();
+		autoCompleteArray = new String[hashSet.size()];
+		Iterator it = hashSet.iterator();
+		int index = 0;
+		while (it.hasNext()) {
+			// Add to string array (for later adapter usage)
+			autoCompleteArray[index++] = (String) it.next();
+		}
+	}
 
-    private void setupSearchBar() {
-        twSummonerName = (AutoCompleteTextView) findViewById(R.id.summoner_name);
-        twSummonerName.requestFocus();
-        twSummonerName.setOnEditorActionListener(new AutoCompleteTextView.OnEditorActionListener() {
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                    if(!searchedPressed && !checkPreviouslySearched()) {
+	/**
+	 * Setups the "Search"-bar and sets previously searched names from SharedPreferences.
+	 * <code>setupSharedPrefs();</code> needs to be called beforehand.
+	 */
+	private void setupSearchBar() {
+		summonerName = (AutoCompleteTextView) findViewById(R.id.summoner_name);
+		summonerName.requestFocus();
+		summonerName.setOnEditorActionListener(new AutoCompleteTextView.OnEditorActionListener() {
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				// Checks if user pressed "Done" on the soft keyboard
+				if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+					if (!searchedPressed && !checkPreviouslySearched()) { // Check if previously searched
 						searchedPressed = true;
-                        btnSearch.setProgress(1);
-                        checkCurrentGame();
-                    }
-                }
-                return false;
-            }
-        });
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, autoCompleteArray);
-        twSummonerName.setAdapter(adapter);
-    }
+						search.setProgress(1);
+						checkCurrentGame();
+					}
+				}
+				return false;
+			}
+		});
+		// Set adapter to the string array from SharedPrefs
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line, autoCompleteArray);
+		summonerName.setAdapter(adapter);
+	}
 
+	/**
+	 * Checks if the previously searched summoner matches the current search, if so, move directly to next intent (data is already stored)
+	 *
+	 * @return <code>boolean</code> - true if previously searched, false if not
+	 */
 	private boolean checkPreviouslySearched() {
 		if (previouslySearchedPatchVersion != null && previouslySearchedSummonerArr != null && previouslySearchedSummoner != null &&
-				previouslySearchedSummoner.equals(twSummonerName.getText().toString().trim()) && previouslySearchedRegion != null) {
+				previouslySearchedSummoner.equals(summonerName.getText().toString().trim()) && previouslySearchedRegion != null) {
 			startIntentToMain(previouslySearchedSummonerArr, previouslySearchedPatchVersion, previouslySearchedSummoner, previouslySearchedRegion);
 			return true;
 		}
@@ -137,29 +163,43 @@ public class LoginActivity extends AppCompatActivity {
 		return false;
 	}
 
+	/**
+	 * Starts a current game check using the information input from the user.
+	 */
 	private void checkCurrentGame() {
-		String summoner = twSummonerName.getText().toString().trim();
-		if (!summoner.isEmpty()) {
-			String region = spRegion.getSelectedItem().toString();
+		String summoner = summonerName.getText().toString().trim();
+		if (!summoner.isEmpty()) { // If summoner name not empty
+			String region = this.region.getSelectedItem().toString();
 
 			GetCurrentGame game = new GetCurrentGame();
-			game.execute(summoner, region);
-		} else {
+			game.execute(summoner, region); // Execute the check, input summoner name and region
+		} else { // Show error
 			Toast.makeText(getApplication(), getString(R.string.enter_name), Toast.LENGTH_LONG).show();
-			btnSearch.setProgress(0);
+			search.setProgress(0);
 			searchedPressed = false;
 		}
 	}
 
+	/**
+	 * Forwards the user to next activity/intent (screen).
+	 *
+	 * @param summonerArr      summoners to be passed with the intent
+	 * @param patchVersion     patch version to be passed with the intent
+	 * @param searchedSummoner summoner name searched
+	 * @param searchedRegion   summoner region
+	 */
 	private void startIntentToMain(Summoner[] summonerArr, String patchVersion, String searchedSummoner, String searchedRegion) {
-        autoCompleteSet.add(searchedSummoner);
-        prefs.edit().putStringSet("searchedNames", autoCompleteSet).apply();
+		// Add summoner name to previously searched list (for autocomplete)
+		autoCompleteSet.add(searchedSummoner);
+		prefs.edit().putStringSet("searchedNames", autoCompleteSet).apply();
+		// Start intent to next activity
 		Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 		Bundle extras = new Bundle();
+		// Set information to pass
 		extras.putSerializable(ARG_SUMMONERS, summonerArr);
 		extras.putString(ARG_VERSION, patchVersion);
 		extras.putString(ARG_SEARCHED_SUMMONER, searchedSummoner);
-        extras.putString(ARG_REGION, searchedRegion);
+		extras.putString(ARG_REGION, searchedRegion);
 		intent.putExtras(extras);
 		startActivity(intent);
 	}
@@ -169,7 +209,8 @@ public class LoginActivity extends AppCompatActivity {
 		View v = getCurrentFocus();
 		boolean ret = super.dispatchTouchEvent(event);
 
-		if (v instanceof EditText) {
+		// Event called whenever a motion (click, focus, touch etc.) is issued
+		if (v instanceof EditText) { // If view matches EditText (in our case the text field, AutoCompleteTextView inherits EditText)
 			View w = getCurrentFocus();
 			if (w != null) {
 				int scrCoords[] = new int[2];
@@ -177,7 +218,9 @@ public class LoginActivity extends AppCompatActivity {
 				float x = event.getRawX() + w.getLeft() - scrCoords[0];
 				float y = event.getRawY() + w.getTop() - scrCoords[1];
 
+				// Check if touch is outside of bounds
 				if (event.getAction() == MotionEvent.ACTION_UP && (x < w.getLeft() || x >= w.getRight() || y < w.getTop() || y > w.getBottom())) {
+					// Hide keyboard
 					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 					imm.hideSoftInputFromWindow(w.getWindowToken(), 0);
 				}
@@ -187,6 +230,10 @@ public class LoginActivity extends AppCompatActivity {
 		return ret;
 	}
 
+	/**
+	 * This nested class is in charge of handling game requests asynchronously,
+	 * allowing a worker thread to do its magic and later updating UI threads.
+	 */
 	private class GetCurrentGame extends AsyncTask<String, Integer, CurrentGame> {
 		private String summonerName;
 		private VersionUtil versionUtil;
@@ -196,20 +243,22 @@ public class LoginActivity extends AppCompatActivity {
 			summonerName = params[0];
 			String region = params[1];
 
-            Context context = getApplicationContext();
-            ResourceUtil resourceUtil = new ResourceUtil(context);
-            JSONParser parser = new JSONParser(resourceUtil);
-            JSONRequester requester = new JSONRequester(parser);
-            versionUtil = new VersionUtil(context, parser, requester, resourceUtil, region);
+			// Initialize parser and requester
+			Context context = getApplicationContext();
+			ResourceUtil resourceUtil = new ResourceUtil(context);
+			JSONParser parser = new JSONParser(resourceUtil);
+			JSONRequester requester = new JSONRequester(parser);
+			versionUtil = new VersionUtil(context, parser, requester, resourceUtil, region);
 
+			// Check if a search is being conducted for the first time, or if patch data is not latest version
 			if (versionUtil.isFirstTime() || !versionUtil.isLatestVersion()) {
-				publishProgress(2);
+				publishProgress(2); // Forwards to onProgressUpdate()
 				versionUtil.updateVersion();
 
 				publishProgress(100);
 
 				publishProgress(3);
-			} else {
+			} else { // Otherwise just update resources
 				try {
 					parser.updateResources();
 				} catch (JSONException e) {
@@ -217,13 +266,15 @@ public class LoginActivity extends AppCompatActivity {
 				}
 			}
 
+			// Conduct a new search
 			CurrentGame game = new CurrentGame(summonerName, region, parser, requester);
 			try {
-                game.searchCurrentGame();
+				game.searchCurrentGame();
 
 				publishProgress(100);
 
 				try {
+					// Sleep to show "Search"-button animation
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
@@ -239,7 +290,7 @@ public class LoginActivity extends AppCompatActivity {
 		@Override
 		protected void onProgressUpdate(Integer... values) {
 			super.onProgressUpdate(values);
-			switch(values[0]) {
+			switch (values[0]) {
 				case 2:
 					Toast.makeText(getApplication(), getString(R.string.patch_data_notcurrent), Toast.LENGTH_LONG).show();
 					break;
@@ -248,25 +299,27 @@ public class LoginActivity extends AppCompatActivity {
 					break;
 			}
 
-			btnSearch.setProgress(values[0]);
+			search.setProgress(values[0]);
 		}
 
 		@Override
 		protected void onPostExecute(CurrentGame result) {
 			super.onPostExecute(result);
-			if (result != null) {
+			// Event fired when worker thread has a result
+			if (result != null) { // Send to MainActivity if data has been retrieved
 				Summoner[] summoners = result.getSummoners();
-				btnSearch.setProgress(0);
-				startIntentToMain(summoners, versionUtil.getVersion(), summonerName, (String) spRegion.getSelectedItem());
-			} else {
-				btnSearch.setProgress(-1);
+				search.setProgress(0);
+				startIntentToMain(summoners, versionUtil.getVersion(), summonerName, (String) region.getSelectedItem());
+			} else { // Display error and reset button if not
+				search.setProgress(-1);
 				Toast.makeText(getApplication(), String.format(getString(R.string.not_in_game), summonerName), Toast.LENGTH_LONG).show();
 
+				// Reset button with a seperate thread (does not work on onPostExecute - it's an UI thread)
 				final Handler handler = new Handler();
 				handler.postDelayed(new Runnable() {
 					@Override
 					public void run() {
-						btnSearch.setProgress(0);
+						search.setProgress(0);
 					}
 				}, 1000);
 			}
--- app/src/main/java/com/grupp32/activity/MainActivity.java
+++ app/src/main/java/com/grupp32/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
@@ -34,9 +33,13 @@ import decoder.ImageStreamDecoder;
 import widget.SlidingTabLayout;
 
 /**
- * @author Alexander Johansson, code samples taken from ObservableScrollView (credits to ksoichiro; https://github.com/ksoichiro/Android-ObservableScrollView)
+ * MainActivity represents a set of summoners. The UI contains of retrieved information from the Riot Games API.
+ * Code samples for a scrollable fragment taken from ObservableScrollView (credits to ksoichiro; https://github.com/ksoichiro/Android-ObservableScrollView).
+ *
+ * @author Alexander Johansson
  */
 public class MainActivity extends AppCompatActivity {
+
 	private SlidingTabLayout tabLayout;
 	private ViewPager viewPager;
 	private NavigationAdapter pagerAdapter;
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 	private Summoner[] summonerArr;
 	private String patchVersion;
 	private String searchedSummoner;
-    private String searchedRegion;
+	private String searchedRegion;
 	private ImageView flexibleImage;
 	private TextView flexibleTitle;
 
@@ -55,55 +58,70 @@ public class MainActivity extends AppCompatActivity {
 	private static final String ARG_SUMMONERS = "ARG_SUMMONERS";
 	private static final String ARG_SEARCHED_SUMMONER = "ARG_SEARCHED_SUMMONERS";
 	private static final String ARG_VERSION = "ARG_VERSION";
-    private static final String ARG_REGION = "ARG_REGION";
+	private static final String ARG_REGION = "ARG_REGION";
 
 	private static final String DDRAGON_CHAMP_SPLASH_URL = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
 
 	@Override
 	public void onBackPressed() {
+		// Event fired when user presses back-button in Android
+
 		resetFragmentTimers();
 
+		// Start intent to move back to LoginActivity, while keeping data in a bundle, for reusing if search matches
 		Intent backIntent = new Intent(MainActivity.this, LoginActivity.class);
 		Bundle extras = new Bundle();
 		extras.putSerializable(ARG_SUMMONERS, summonerArr);
 		extras.putString(ARG_VERSION, patchVersion);
 		extras.putString(ARG_SEARCHED_SUMMONER, searchedSummoner);
-        extras.putString(ARG_REGION, searchedRegion);
+		extras.putString(ARG_REGION, searchedRegion);
 		backIntent.putExtras(extras);
-		backIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		backIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // For popping backstack, user cannot navigate back to this activity through back
 		startActivity(backIntent);
 	}
 
 	@Override
 	public void onPause() {
+		// Toggles sound based on app pause (disables the sound from triggering when app is in background, resumes when in foreground)
 		toggleFragmentSound();
 		super.onPause();
 	}
 
 	@Override
 	public void onResume() {
+		// Toggles sound based on app resume (disables the sound from triggering when app is in background, resumes when in foreground)
 		toggleFragmentSound();
 		super.onResume();
 	}
 
-	private void toggleFragmentSound(){
+	/**
+	 * Toggles sound enabled/disabled for the app. Forwards the toggle to all child fragments
+	 */
+	private void toggleFragmentSound() {
+		// Get all child fragments
 		FragmentManager fragmentManager = MainActivity.this.getSupportFragmentManager();
 		List<Fragment> fragments = fragmentManager.getFragments();
-		if(fragments != null) {
-			for (Fragment fragment : fragments) {
-				if (fragment != null) {
+		if (fragments != null) {
+			for (Fragment fragment : fragments) { // For every fragment
+				if (fragment != null) { // Not null
+					// Toggle sound in TabFragment
 					((TabFragment) fragment).toggleSound();
 				}
 			}
 		}
 	}
 
-	private void resetFragmentTimers(){
+	/**
+	 * Resets timers in all fragments for the app. Forwards to child fragments.
+	 */
+	private void resetFragmentTimers() {
+		// Get all child fragments
 		FragmentManager fragmentManager = MainActivity.this.getSupportFragmentManager();
 		List<Fragment> fragments = fragmentManager.getFragments();
-		if(fragments != null) {
-			for(Fragment fragment : fragments) {
-				if(fragment != null) {
+		if (fragments != null) {
+			for (Fragment fragment : fragments) { // For every fragment
+				if (fragment != null) { // Not null
+					// Reset timers in TabFragment
 					((TabFragment) fragment).resetTimers();
 				}
 			}
@@ -113,29 +131,34 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Set root view
 		setContentView(R.layout.activity_main);
 
 		flexibleImage = (ImageView) findViewById(R.id.image);
 		flexibleTitle = (TextView) findViewById(R.id.title);
 
+		// Catch bundle from LoginActivity
 		Intent intent = this.getIntent();
 		Bundle bundle = intent.getExtras();
-		if(bundle != null) {
+		if (bundle != null) {
+			// Get serialized data from bundle
 			summonerArr = (Summoner[]) bundle.getSerializable(ARG_SUMMONERS);
 			patchVersion = bundle.getString(ARG_VERSION);
 			searchedSummoner = bundle.getString(ARG_SEARCHED_SUMMONER);
-            searchedRegion = bundle.getString(ARG_REGION);
+			searchedRegion = bundle.getString(ARG_REGION);
+			// Add summoners
 			for (Summoner summoner : summonerArr) {
 				summoners.add(summoner);
 			}
 		}
 
+		// Create a view pager (tabs) and set its adapter to the pager (which has summoner information passed)
 		pagerAdapter = new NavigationAdapter(getSupportFragmentManager());
-		pagerAdapter.setSummoners(summoners);
-		pagerAdapter.setPatchVersion(patchVersion);
+		pagerAdapter.setSummoners(summoners); // Pass summoner info
+		pagerAdapter.setPatchVersion(patchVersion); // Pass version info
 		viewPager = (ViewPager) findViewById(R.id.pager);
 		viewPager.setAdapter(pagerAdapter);
-		viewPager.setOffscreenPageLimit(5);
+		viewPager.setOffscreenPageLimit(5); // Set this to cache all 5 fragments, avoid reloading them
 		viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 			@Override
 			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -143,11 +166,14 @@ public class MainActivity extends AppCompatActivity {
 
 			@Override
 			public void onPageSelected(int position) {
+				// Event fires when a tab is selected
 				try {
+					// Switch flexible image
 					new ImageSwitcher().execute(
-                            new URL(DDRAGON_CHAMP_SPLASH_URL + summoners.get(position).getChampion().getKey() + "_0.jpg")
-                    );
+							new URL(DDRAGON_CHAMP_SPLASH_URL + summoners.get(position).getChampion().getKey() + "_0.jpg")
+					);
 
+					// Set flexible title
 					flexibleTitle.setText(summoners.get(position).getName());
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
@@ -162,9 +188,7 @@ public class MainActivity extends AppCompatActivity {
 		flexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
 		tabHeight = getResources().getDimensionPixelSize(R.dimen.tab_height);
 
-		TextView titleView = (TextView) findViewById(R.id.title);
-		titleView.setText(R.string.app_name);
-
+		// Setup the tab layout
 		tabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
 		tabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
 		tabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
@@ -174,10 +198,12 @@ public class MainActivity extends AppCompatActivity {
 		ScrollUtils.addOnGlobalLayoutListener(tabLayout, new Runnable() {
 			@Override
 			public void run() {
+				// Translates the tab
 				translateTab(0, false);
 			}
 		});
 
+		// Set view pager to 2 so it scrolls to the middle automatically
 		viewPager.setCurrentItem(2);
 	}
 
@@ -186,6 +212,8 @@ public class MainActivity extends AppCompatActivity {
 	 * They all call this method even when they are inactive
 	 * but this Activity should listen only the active child,
 	 * so each Fragments will pass themselves for Activity to check if they are active.
+	 * <p/>
+	 * Credits to ksoichiro
 	 *
 	 * @param scrollY scroll position of Scrollable
 	 * @param s       caller Scrollable view
@@ -213,6 +241,13 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	/**
+	 * Sets current scrolled Y-axis
+	 * <p/>
+	 * Credits to ksoichiro
+	 *
+	 * @param scrollY y-axis
+	 */
 	private void propagateScroll(int scrollY) {
 		// Set scrollY for the fragments that are not created yet
 		pagerAdapter.setScrollY(scrollY);
@@ -239,6 +274,14 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	/**
+	 * Animates and displays views depending on the scroll axis.
+	 * <p/>
+	 * Credits to ksoichiro
+	 *
+	 * @param scrollY
+	 * @param animated
+	 */
 	private void translateTab(int scrollY, boolean animated) {
 		int flexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
 		int flexTabHeight = getResources().getDimensionPixelSize(R.dimen.tab_height);
@@ -283,6 +326,11 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	/**
+	 * Retrieves current actionbar size.
+	 *
+	 * @return <code>int</code> - actionbar size
+	 */
 	protected int getActionBarSize() {
 		TypedValue typedValue = new TypedValue();
 		int[] textSizeAttr = new int[]{R.attr.actionBarSize};
@@ -293,33 +341,58 @@ public class MainActivity extends AppCompatActivity {
 		return actionBarSize;
 	}
 
+	/**
+	 * This nested class handles information about summoners for the seperate tabs.
+	 * The adapter creates the fragment upon request (when selecting the tab).
+	 */
 	private static class NavigationAdapter extends CacheFragmentStatePagerAdapter {
 		private ArrayList<Summoner> summoners = new ArrayList<Summoner>();
 		private String patchVersion;
 
 		private int scrollY;
 
+		/**
+		 * Constructor for this adapter, sets its superclass FragmentManager.
+		 *
+		 * @param fm fragment manager
+		 */
 		public NavigationAdapter(FragmentManager fm) {
 			super(fm);
 		}
 
+		/**
+		 * Sets the current summoners for the adapter to use.
+		 *
+		 * @param summoners array list with Summoner-objects
+		 */
 		public void setSummoners(ArrayList<Summoner> summoners) {
 			this.summoners = summoners;
 		}
 
+		/**
+		 * Sets the current patch version for the adapter to use.
+		 *
+		 * @param patchVersion patch version
+		 */
 		public void setPatchVersion(String patchVersion) {
 			this.patchVersion = patchVersion;
 		}
 
+		/**
+		 * Sets the Y-axis for the adapter to propagate.
+		 *
+		 * @param scrollY y-axis
+		 */
 		public void setScrollY(int scrollY) {
 			this.scrollY = scrollY;
 		}
 
 		@Override
 		protected Fragment createItem(int position) {
+			// Creates a fragment and sets its arguments
 			FlexibleSpaceFragment fragment;
 			fragment = new TabFragment();
-			fragment.setArguments(scrollY, summoners.get(position), patchVersion);
+			fragment.setArguments(scrollY, summoners.get(position), patchVersion); // Pass summoner based on tab position, pass patch version
 			return fragment;
 		}
 
@@ -334,11 +407,17 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	/**
+	 * This nested class handles changing of the image for the flexible image view.
+	 * This is handled asynchronously.
+	 */
 	private class ImageSwitcher extends AsyncTask<URL, Bitmap, Bitmap> {
 		protected Bitmap doInBackground(URL... urls) {
+			// Init a bitmap and the decoder
 			Bitmap bitmap = null;
 			ImageStreamDecoder decoder = new ImageStreamDecoder();
 			try {
+				// Try to decode from given URL (input stream), use decoder to load the bitmap into memory efficiently
 				bitmap = decoder.decodeSampledBitmapFromStream((InputStream) urls[0].getContent(), 300, 240);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -348,7 +427,9 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		protected void onPostExecute(Bitmap bitmap) {
-			if(bitmap != null) {
+			// If bitmap not null
+			if (bitmap != null) {
+				// Set image
 				flexibleImage.setImageBitmap(bitmap);
 			}
 		}
